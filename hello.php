8414f35c4224d6b19c843aaa07cbc823669c833a
juliusbenson <juliusbenson@users.noreply.github.com>
2016-05-05 21:08:21 -0500

    added web3 stuff
    I can't get web3 to connect to mist yet, but I can call functions in web3 from the chrome inspector's console (try it out)

--- hello.php
+++ hello.php
@@ -1,3 +1,10 @@
+<script src="web3.min.js"></script>
+<script>
+ var Web3 = require('web3');
+ var web3 = new Web3();
+ console.log(web3);
+ web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+</script>
 <html>
  <head>
   <title>PHP Test</title>
