a576f4291bd4295a863182e8d187a2704ca5bd6f
supermaxio <maxiomeier64@gmail.com>
2016-05-19 20:32:46 -0500

    Updated to meet solidity syntax
    Get and set should work now.
    
    Count still in progress.

--- republickey.sol
+++ republickey.sol
@@ -1,76 +1,75 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf460
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+/*
+ * Authors: M.J.Meier, J.S.Benson, R.K.Johnson
+ * RepublicKey contract will allow votes to be set and counted and the winning
+ *              proposal can be obtained.
+ */
+contract RepublicKey
+{
+    bytes32 private emptyHash;
+    bytes32 private hProposalWinner;
+    uint private numberOfVoters;
+    address[] private listOfVoters;
+    
+    // votes are the hash of the voters as the key,
+    // and the value is the hash of the proposal.
+    mapping (address => bytes32) votes;
+    
+    // proposals are the hash of the proposal as the key,
+    // and the value is the number of votes for it.
+    mapping (bytes32 => uint) proposals;
 
-\f0\fs24 \cf0 /*\
- * Authors: M.J.Meier, J.S.Benson, R.K.Johnson\
- * RepublicKey contract will allow votes to be set and counted and the winning\
- *              proposal can be obtained.\
- */\
-contract RepublicKey\
-\{\
-    string public proposalWinner;\
-    Vote[] public votes;\
-    uint public numberOfVoters;\
-    CountOfProposal[] public proposals;\
-    \
-    struct Vote \{\
-        address voterHash;\
-        string proposalVotedOn;\
-    \} \
-    \
-    struct CountOfProposal \{\
-        string proposalVotedOn;\
-        uint numberOfVotes;\
-    \}\
-    \
-    /*\
-     * Constructor\
-     */\
-    function RepublicKey()\
-    \{\
-        numberOfVoters = 0;\
-        proposalWinner = '';\
-        \
-    \}\
-    \
-    /*\
-     * Set - Will set the voterHash and the proposal string they voted on in\
-     *      the votes array.\
-     */\
-    function Set(address voterHash, string proposalVotedOn)\
-    \{\
-        // This checks if voter has already voted\
-        for (uint i = 0; i < numberOfVoters; i++) \{\
-            if (votes[i].voterHash == voterHash) \{\
-                return;\
-            \}\
-        \}\
-        \
-        votes[numberOfVoters] = Vote(\{\
-            voterHash: voterHash,\
-            proposalVotedOn: proposalVotedOn\
-        \});\
-        numberOfVoters++;\
-    \}\
-    \
-    /*\
-     * Get - Simply gets the proposal winner string run by the Count function.\
-     */\
-    function Get()\
-    returns (string proposalWinner)\
-    \{\
-        return proposalWinner;\
-    \}\
-    \
-    //Not done with this function yet..\
-    function Count()\
-    \{\
-        uint numberOfProposals = 0;\
-        for (uint i = 0; i < numberOfVoters; i++) \{\
-            //proposals[numberOfProposals] = \
-        \}\
-    \}\
-\}}
+    /*
+     * Constructor
+     */
+    function RepublicKey()
+    {
+        numberOfVoters = 2;
+        hProposalWinner = emptyHash;
+        listOfVoters[0] = 0xc3453546fA7d38be9FceCAE590243CC4B85fdF28;
+        listOfVoters[1] = 0x5065E18c3Fa4e7BB4f125689Aff461bB40Bf9cBc;
+        
+        for (uint i = 0; i < numberOfVoters; i++) {
+            votes[listOfVoters[0]] = emptyHash;
+        }
+    }
+    
+    /*
+     * Set - Will set the voterHash and the proposal string they voted on in
+     *      the votes array.
+     */
+    function Set(bytes32 proposalVotedOn)
+    {
+        // This checks if the voter can vote
+        for (uint i = 0; i < numberOfVoters; i++) {
+            if (listOfVoters[i] == msg.sender) {
+                break;
+            }
+            
+            if (i == numberOfVoters - 1) {
+                return;
+            }
+        }
+        
+        // This checks if voter has already voted
+        if (votes[msg.sender] != emptyHash) {
+            return;
+        }
+        
+        votes[msg.sender] = proposalVotedOn;
+    }
+    
+    /*
+     * Get - Simply gets the proposal winner string run by the Count function.
+     */
+    function Get()
+    returns (bytes32 proposalWinner)
+    {
+        return hProposalWinner;
+    }
+    
+    //Not done with this function yet..
+    function Count()
+    {
+        
+    }
+}
