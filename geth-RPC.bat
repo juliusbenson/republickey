2835e3ca8996a4f96a660943424279b7b1c09fb2
juliusbenson <juliusbenson@users.noreply.github.com>
2016-05-19 20:32:57 -0500

    Update geth-RPC.bat
    To add an address to your geth instance, type "geth account new" in the console (no quotes)

--- geth-RPC.bat
+++ geth-RPC.bat
@@ -1,1 +1,1 @@
-geth --mine --etherbase="0xb00223c746493b23063c5a839c92a8f69c260642" --rpc --rpccorsdomain="*" --datadir=".\data" --genesis="customgenesis.json" --networkid 1100 -nodiscover -maxpeers 0 console 2>>geth.log
+geth --mine --etherbase="make sure to use your own etherbase address" --rpc --rpccorsdomain="*" --datadir=".\data" --genesis="customgenesis.json" --networkid 1100 -nodiscover -maxpeers 0 console 2>>geth.log
