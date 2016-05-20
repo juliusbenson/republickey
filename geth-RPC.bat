3c6db3c7aa183639dfed7c14a132fbde951ec97e
juliusbenson <juliusbenson@users.noreply.github.com>
2016-05-19 20:29:18 -0500

    web3 testing stuff
    I'm just going to put web3 odds and ends in here while we get oriented to it. This code should properly connect to your geth node running according to geth-rpc.bat. You need to use customgenesis.json to boot up a new blockchain for your private testnet

---
+++ geth-RPC.bat
@@ -0,0 +1,1 @@
+geth --mine --etherbase="0xb00223c746493b23063c5a839c92a8f69c260642" --rpc --rpccorsdomain="*" --datadir=".\data" --genesis="customgenesis.json" --networkid 1100 -nodiscover -maxpeers 0 console 2>>geth.log
