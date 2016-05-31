44342e7c6b31f8df6cb6e6fabe91a102c64263bc
juliusbenson <juliusbenson@users.noreply.github.com>
2016-05-30 21:42:58 -0500

    Add files via upload
    The greeterDeploy file simply contains all the commands you need to enter into geth to deploy the greeter.
    I've modified the geth-RPC batch file a little, I took out the --mine flag.
    Also fixed a typo in customgenesis.json

--- geth-RPC.bat
+++ geth-RPC.bat
@@ -1,1 +1,1 @@
-geth --mine --etherbase="make sure to use your own etherbase address" --rpc --rpccorsdomain="*" --datadir=".\data" --genesis="customgenesis.json" --networkid 1100 -nodiscover -maxpeers 0 console 2>>geth.log
+geth --etherbase="0xf4e4670d540ab36821426df6ecd16ccd538c743e" --rpc --rpccorsdomain="*" --datadir=".\data" --genesis="customgenesis.json" --networkid 1100 -nodiscover -maxpeers 0 console 2>>geth.log
